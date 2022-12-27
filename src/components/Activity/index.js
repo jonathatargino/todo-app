ad3179297a9ed934bc7847b3d445f32a8e318537
Jonatha Targino <j.oliveiratargino@hotmail.com>
2022-12-27 14:29:51 -0300

    Activity components receives doneState

--- src/components/Activity/index.js
+++ src/components/Activity/index.js
@@ -3,8 +3,8 @@ import Checkbox from "../Checkbox";
 import "./style.css"
 import ActivityTitle from "../ActivityTitle";
 
-function Activity({title}){
-  const [isDone, setIsDone] = useState(false);
+function Activity({title, doneState}){
+  const [isDone, setIsDone] = useState(doneState);
   return (
     <div className="todoActivity">
       <Checkbox onClick={() => isDone? setIsDone(false) : setIsDone(true)}/>
