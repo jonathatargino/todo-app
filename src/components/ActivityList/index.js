0381985daa60d41f66cd165bb683d51681f8203b
Jonatha Targino <j.oliveiratargino@hotmail.com>
2022-12-27 14:28:37 -0300

    ActivityList component created

---
+++ src/components/ActivityList/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+function ActivityList({children}){
+  return (
+    <div id="activityList">
+      {children}
+    </div>
+  )
+}
+
+export default ActivityList
