2d9d28869c4799f26b370ba6dbd399e8394cf87c
Jonatha Targino <j.oliveiratargino@hotmail.com>
2022-12-27 14:31:04 -0300

    ActivityForm accessing the input data and putting it in activities state

--- src/components/ActivityForm/index.js
+++ src/components/ActivityForm/index.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css"
 
-function ActivityForm() {
+function ActivityForm({activities, setActivities}) {
+  const [newActivity, setNewActivity] = useState('');
+
+  function handleSubmit(event){
+    event.preventDefault();
+    setActivities([
+      ...activities,
+      {
+        title: newActivity,
+        isDone: false,
+      }
+    ])
+    event.target.reset();
+  }
+
   return (
-    <form id="activityForm">
-      <input className="todoInput" placeholder="Adicione uma nova atividade..." autoComplete="off"/>
+    <form 
+      id="activityForm"
+      onSubmit={(event) => handleSubmit(event)}
+    >
+      <input 
+        className="todoInput" 
+        placeholder="Adicione uma nova atividade..." 
+        autoComplete="off"
+        onChange={(event) => setNewActivity(event.target.value)}
+      />
+
     </form>
   )
 }
