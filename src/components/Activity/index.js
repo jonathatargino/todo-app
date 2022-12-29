3b8af8092dd366be813def256ceae32ad161c47e
Jonatha Targino <j.oliveiratargino@hotmail.com>
2022-12-29 16:33:05 -0300

    Checkbox onClick changing activities state

--- src/components/Activity/index.js
+++ src/components/Activity/index.js
@@ -3,11 +3,25 @@ import Checkbox from "../Checkbox";
 import "./style.css"
 import ActivityTitle from "../ActivityTitle";
 
-function Activity({title, doneState}){
+function Activity({title, doneState, setActivities, activities}){
   const [isDone, setIsDone] = useState(doneState);
   return (
     <div className="todoActivity">
-      <Checkbox onClick={() => isDone? setIsDone(false) : setIsDone(true)}/>
+      <Checkbox 
+        onClick={
+          () => {
+            isDone? setIsDone(false) : setIsDone(true)
+            setActivities(activities.map(activity => {
+              if (activity.title === title){
+                activity.isDone? activity.isDone = false : activity.isDone = true;
+              }
+              
+              return activity;
+            }))
+          }
+        }
+        checked={isDone}
+      />
       <ActivityTitle title={title} isDone={isDone}/>
     </div>
   )
