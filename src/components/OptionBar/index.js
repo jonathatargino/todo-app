f0bac313f507aeb0d1a7c2d5b33dcfe21ebae81f
Jonatha Targino <j.oliveiratargino@hotmail.com>
2022-12-29 00:51:05 -0300

    OptionBar component created

---
+++ src/components/OptionBar/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import Option from "../../Option";
+import "./style.css"
+
+function OptionBar({selectedOption, setSelectedOption}){
+
+  return(
+    <div className="optionBar">
+      <ul className="options">
+        <Option 
+          optionName="all" 
+          selectedOption={selectedOption} 
+          setSelectedOption={setSelectedOption}
+        >
+          Todas
+        </Option>
+        <Option 
+          optionName="active" 
+          selectedOption={selectedOption} 
+          setSelectedOption={setSelectedOption}
+        >
+          Pendentes
+        </Option>
+        <Option 
+          optionName="completed" 
+          selectedOption={selectedOption} 
+          setSelectedOption={setSelectedOption}
+        >
+          Concluidas
+        </Option>
+      </ul>
+    </div>
+  )
+}
+
+export default OptionBar
