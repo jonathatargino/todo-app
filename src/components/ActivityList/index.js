f98d4af2154e194ff92cebf1a79bc7583cbaab85
Jonatha Targino <j.oliveiratargino@hotmail.com>
2022-12-29 16:37:52 -0300

    ActivityList now rendering activities according the optionSelected

--- src/components/ActivityList/index.js
+++ src/components/ActivityList/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+
+import Activity from "../Activity";
 import "./style.css"
 
-function ActivityList({children}){
+function ActivityList({activities, setActivities, selectedOption}){
+  let selectedActivities = [];
+
+  if (selectedOption === "all"){
+    selectedActivities = [...activities]
+  }
+
+  if (selectedOption === "active"){
+    selectedActivities = activities.filter(activity => !activity.isDone)
+  }
+
+  if (selectedOption === "completed"){
+    selectedActivities = activities.filter(activity => activity.isDone)
+  }
+
   return (
     <div id="activityList">
-      {children}
+      {selectedActivities.map((activity) => {
+          return(
+            <Activity 
+              title={activity.title} 
+              doneState={activity.isDone} 
+              activities={activities}
+              setActivities={setActivities}
+              key={activity.title}
+            />
+          )
+        })}
     </div>
   )
 }
